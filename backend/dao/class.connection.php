80baa94bcd55916615c864c4eeaa7f404cf2b3b6
cbcarlos07 <cbcarlos08@gmail.com>
2020-07-27 22:39:19 -0400

    first commit

---
+++ backend/dao/class.connection.php
@@ -0,0 +1,30 @@
+<?php
+
+class ConnectionFactory extends PDO{
+    private $dsn = 'mysql:host=mysql.;dbname=meus_locais';
+
+    private $user = 'root';
+    private $password = '123';
+    public $handle = null;
+
+    function __construct() {
+        try {
+            //aqui ela retornará o PDO em si, veja que usamos parent::_construct()
+            if ($this->handle == null) {
+                $dbh = parent::__construct($this->dsn, $this->user, $this->password);
+                $this->handle = $dbh;
+                return $this->handle;
+            }
+        } catch (PDOException $e) {
+            echo 'Conexão falhou. Erro: ' . $e->getMessage();
+            return false;
+        }
+    }    
+
+         //aqui criamos um objeto de fechamento da conexão
+    function __destruct() {
+        $this->handle = NULL;
+    }
+}
+
+?>
